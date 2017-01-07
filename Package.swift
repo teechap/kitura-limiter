949f11a74ed34b9321ab0fa5ed3fef349f398aad
Travis <travisechapman@gmail.com>
2017-01-07 18:16:28 -0500

    package init with type=library

--- Package.swift
+++ Package.swift
@@ -2,7 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "KituraLimiter",
-    targets: [],
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
         .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1, minor: 4),
