5ebe43dfe68eb33a93c078738c4502c946c020f9
Travis <travisechapman@gmail.com>
2017-01-07 17:56:45 -0500

    Name change

--- Package.swift
+++ Package.swift
@@ -1,7 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "kitura-limiter",
+    name: "KituraLimiter",
+    targets: [],
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
         .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1, minor: 4),
