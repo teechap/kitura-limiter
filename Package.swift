5f8d5c1f973043dc4b0ff2aa41a39f9f72d2f5df
Karl Weinmeister <kweinmeister@us.ibm.com>
2017-07-12 09:38:30 -0500

    Updated .swift_version and Package.swift to support Swift 3.1.1

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "KituraLimiter",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1, minor: 4),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 4)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1)
     ]
 )
